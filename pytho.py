910efa89935e4cc8c419008872686c8f6892532c
parsa-cyber <69917768+parsa-cyber@users.noreply.github.com>
2020-08-22 18:59:43 -0400

    Create pytho.py

---
+++ pytho.py
@@ -0,0 +1,8 @@
+a = float(input("enter the length of first side"))
+
+b = float(input("enter the length of Second side"))
+
+c_squared = a**2 + b**2
+
+c = c_squared**0.5
+print("the length of hypotnuse is",c)
